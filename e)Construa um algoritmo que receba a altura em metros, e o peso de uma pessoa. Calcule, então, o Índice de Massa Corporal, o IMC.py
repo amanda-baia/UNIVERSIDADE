a47f9cc68f6ac5fddf3ae5a437b453ebed3a5978
amanda-baia <amandabaiaadv@gmail.com>
2026-04-04 22:55:10 -0300

    Delete e)Construa um algoritmo que receba a altura em metros, e o peso de uma pessoa. Calcule, então, o Índice de Massa Corporal, o IMC.py

--- e)Construa um algoritmo que receba a altura em metros, e o peso de uma pessoa. Calcule, então, o Índice de Massa Corporal, o IMC.py
+++
@@ -1,7 +0,0 @@
-#e) Construa um algoritmo que receba a altura em metros, e o peso de uma pessoa. Calcule, então, o Índice de Massa Corporal, o IMC:
-
-h = float(input('digite a altura em metros: '))
-p = float(input('digite a peso em kg: '))
-
-IMC = p/(h*h)
-print ("O valor do IMC é:", IMC)
