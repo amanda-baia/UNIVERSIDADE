53a773812d9fd129586c7b4b2ddb40da23f59c4b
amanda-baia <amandabaiaadv@gmail.com>
2026-04-04 22:55:18 -0300

    Delete d) Elabore um algoritmo que receba dois números e apresente o resultado da subtração entre eles.py

--- d) Elabore um algoritmo que receba dois números e apresente o resultado da subtração entre eles.py
+++
@@ -1,6 +0,0 @@
-# d) Elabore um algoritmo que receba dois números e apresente o resultado da subtração entre eles:
-
-n1 = float(input('digite o primeiro número'))
-n2 = float(input('digite o segundo número'))
-S = n1-n2
-print ("O valor final da subtração dos valores é:", S)
